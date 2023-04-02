b58cd06b2e48f8db2e4616f83e05beccd243f87b
Aliev Nurlan <nualiev420@gmail.com>
2023-04-02 22:35:55 +0400

    refactor code

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,62 @@
-from flask import Flask, render_template
+import os
+
+from flask import (Flask, url_for,
+                   render_template, get_flashed_messages,
+                   request, redirect, flash)
+from page_analyzer.main import add_site, create_table, find_id, find_name
+from dotenv import load_dotenv
+from validators.url import url
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# create_table()
 
 
 @app.route('/')
 def link():
-    return render_template('main_page.html')
+    messages = get_flashed_messages(with_categories=True)
+    return render_template('main_page.html', messages=messages)
 
 
-@app.route('/urls', methods=['GET', 'POST'])
-def urls():
-    pass
+@app.get('/urls')
+def get_urls():
+    return render_template('urls.html')
+
+
+@app.post('/urls')
+def post_urls():
+
+    site = request.form.get('url')
+    if len(site) < 1:
+        flash('URL обязателен', 'error')
+
+    if not url(site):
+        flash('Некорректный URL', 'error')
+        return redirect(url_for('link'))
+
+    elif len(site) <= 255:
+        id = find_id(site)
+        if not id:
+            add_site(site)
+            flash('Страница успешно добавлена', 'success')
+            id = find_id(site)
+            return redirect(f'/urls/<{id[0]}>')
+        else:
+            flash('Страница уже существует', 'info')
+            return redirect(f'/urls/<{id[0]}>')
+
+    else:
+        flash('URL превышает 255 символов', 'error')
+        return redirect(url_for('link'))
+
+
+@app.route('/urls/<id>')
+def urls_id(id):
+    id = id[1]
+    messages = get_flashed_messages(with_categories=True)
+    name = find_name(id)[0]
+    return render_template('urls_id.html', messages=messages, name=name)
