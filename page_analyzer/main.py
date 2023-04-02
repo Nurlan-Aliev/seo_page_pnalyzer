2c8faae6af2a16503e47bcc3c1496dffbd86302f
Aliev Nurlan <nualiev420@gmail.com>
2023-04-02 22:35:01 +0400

    add page_analyzer/main.py

---
+++ page_analyzer/main.py
@@ -0,0 +1,46 @@
+import psycopg2
+import os
+from datetime import datetime
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+
+
+def create_table():
+    with conn.cursor() as cur:
+        cur.execute('DROP TABLE IF EXISTS urls;')
+        cur.execute('''CREATE TABLE urls (
+        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(255) UNIQUE,
+        created_at TIMESTAMP);''')
+        conn.commit()
+
+
+def add_site(site):
+    normalize = urlparse(site)
+    home_page = f'{normalize.scheme}://{normalize.netloc}'
+    with conn.cursor() as cur:
+        cur.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s);', (home_page, datetime.now()))
+        conn.commit()
+
+
+def find_id(site):
+    normalize = urlparse(site)
+    home_page = f'{normalize.scheme}://{normalize.netloc}'
+    with conn.cursor() as cur:
+        cur.execute("SELECT id FROM urls WHERE name = %s;", (home_page,))
+        id = cur.fetchone()
+    return id
+
+
+def find_name(id):
+    with conn.cursor() as cur:
+        cur.execute("SELECT name FROM urls WHERE id = %s;", (id,))
+        name = cur.fetchone()
+    return name
