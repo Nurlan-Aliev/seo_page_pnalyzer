90ade64e21fd90298ab9d0f45c4ea7c3f1f97a2f
Aliev Nurlan <nualiev420@gmail.com>
2023-05-02 20:26:42 +0400

    refactor database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,16 @@
-DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS urls, url_checks;
 
 CREATE TABLE urls (
-id INTEGER PRIMARY KEY,
+id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(255) UNIQUE,
+created_at DATE);
+
+
+CREATE TABLE url_checks (
+id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+url_id INTEGER,
+status_code INTEGER,
+h1 VARCHAR(255),
+title VARCHAR(255),
+description TEXT,
 created_at DATE);
