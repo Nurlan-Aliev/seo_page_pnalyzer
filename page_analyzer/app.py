ced6cb33d861b25fbb5beb6e8eb2c53a739ca28a
Aliev Nurlan <nualiev420@gmail.com>
2023-05-02 20:29:39 +0400

    creat of site check logic

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,8 +2,10 @@ import os
 from flask import (Flask, url_for,
                    render_template, get_flashed_messages,
                    request, redirect, flash, session)
-from page_analyzer.main import (add_site, create_table,
-                                find_id, find_site, select_all)
+from page_analyzer.main import (create_table_urls,
+                                add_site, find_id, find_site,
+                                select_from_urls, create_table_checks,
+                                select_from_check, add_check)
 from dotenv import load_dotenv
 from validators.url import url
 
@@ -13,20 +15,26 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['PERMANENT_SESSION_LIFETIME'] = 432000
 
 
 @app.route('/')
 def link():
     if session.get('table') is None:
-        session['table'] = create_table()
+        create_table_urls()
+        session['table'] = True
     messages = get_flashed_messages(with_categories=True)
     return render_template('main_page.html', messages=messages)
 
 
 @app.get('/urls')
 def get_urls():
-    table = select_all()
-    return render_template('urls.html', table=table)
+    table = select_from_urls()
+
+    context = {
+        'select_from_check': select_from_check
+    }
+    return render_template('urls.html', table=table, **context)
 
 
 @app.post('/urls')
@@ -58,6 +66,18 @@ def post_urls():
 
 @app.route('/urls/<id>')
 def urls_id(id):
+    if session.get('check') is None:
+        create_table_checks()
+        session['check'] = True
     messages = get_flashed_messages(with_categories=True)
     site = find_site(id)
-    return render_template('urls_id.html', messages=messages, site=site)
+    checks_url = select_from_check(id)
+    return render_template('urls_id.html', messages=messages,
+                           site=site, checks_url=checks_url)
+
+
+@app.post('/urls/<id>/checks')
+def check(id):
+    add_check(id)
+    flash('Страница успешно проверена', 'success')
+    return redirect(f'/urls/{id}'), 302
